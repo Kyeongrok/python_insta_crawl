d8114c3cd224c94f87436a488b39d5182d1976c8
kyeongrok <oceanfog1@gmail.com>
2019-05-04 12:01:09 +0900

    parse

---
+++ libs/instagram/parser.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+def getPostInfo(post):
+    aTag = post.find("a")
+    link = "https://www.instagram.com{}".format(aTag["href"])
+    return {"link":link}
+
+def parse(pageString):
+    bsObj = BeautifulSoup(pageString, "html.parser")
+
+    posts = bsObj.findAll("div", {"class":"v1Nh3"})
+    postInfos = []
+
+    for post in posts:
+        postInfo = getPostInfo(post)
+        print(postInfo)
+
+    print(len(posts))
+
