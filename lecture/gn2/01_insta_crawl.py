47876b2f250eac0986c13be456f4f17886ad9ad3
kyeongrok <oceanfog1@gmail.com>
2019-12-14 11:13:19 +0900

    댓글 달기 with random sec

--- lecture/gn2/01_insta_crawl.py
+++ lecture/gn2/01_insta_crawl.py
@@ -1,7 +1,7 @@
 # selenium으로 instagram열기
 
 from selenium import webdriver
-import time
+import time, random
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome(
@@ -16,30 +16,50 @@ driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div
 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys("1234@Asdf")
 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div').click()
 
-time.sleep(3)
+# time.sleep(3)
 # popup
-driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
+# driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
 
 # search
 time.sleep(2)
-url = "https://www.instagram.com/explore/tags/축구/"
+url = "https://www.instagram.com/explore/tags/성형/"
 driver.get(url)
 
-time.sleep(2)
 
 def parse(pageString):
     bsobj = BeautifulSoup(pageString, "html.parser")
     ezdmt = bsobj.find("div", {"class":"EZdmt"})
     v1Nh3List = ezdmt.findAll("div", {"class":"v1Nh3"})
 
+    links = []
     for v1Nh3 in v1Nh3List:
         instaLink = "https://www.instagram.com"
         # <a href="123" alt="456">hi my name is ~~</a>
         linkAddr = v1Nh3.find("a")['href']
-        print(instaLink + linkAddr)
+        links.append(instaLink + linkAddr)
 
+    return links
+
+time.sleep(3)
 pageString = driver.page_source
 links = parse(pageString)
-print(links)
 
+# 좋아요 누르고 댓글 달기
+for url in links:
+    try:
+        print(url)
+        driver.get(url)
+
+        rndSec = random.randint(5, 15)
+        time.sleep(rndSec)
+        message = "잘 보고 갑니다. 오늘 날씨가 너무 좋네요. 제 인스타도 놀러와주세요. 맞팔 신청합니다."
+        #driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/section[1]/span[1]/button').click()
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/section[3]/div/form/textarea').click()
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/section[3]/div/form/textarea').send_keys(message)
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/section[3]/div/form/button').click()
+    except Exception as e:
+        pass
+
+
+# driver.close()
 # link뽑기
